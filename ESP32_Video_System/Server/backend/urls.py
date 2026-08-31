c7d8dd808ae033be5bf6cdfb44b93cbb8d17ac8c
Victor Rafael Santana Pimentel <victorrafaelsantana@hotmail.es>
2026-08-30 21:04:46 -0600

    fixed and updated some bugs

--- ESP32_Video_System/Server/backend/urls.py
+++ ESP32_Video_System/Server/backend/urls.py
@@ -17,7 +17,8 @@ urlpatterns = [
     path('api/signup/', views.api_signup, name='api_signup'), 
     path('api/login/', views.api_login, name='api_login'),
     path('api/vault/save/<int:video_id>/', views.save_to_vault, name='save_to_vault'),
-    path('api/vault/delete/<int:video_id>/', views.delete_video, name='delete_video'), 
+    path('api/vault/delete/<int:video_id>/', views.delete_video, name='delete_video'),
+    path('api/camera/create/', views.generate_camera_id, name='generate_camera'),
 ]
 
 if settings.DEBUG:
